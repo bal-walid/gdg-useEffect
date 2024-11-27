86a73286d2d45a56257dbbf8998b320bcaf1162a
bal-wal <balhaoui.walid@ensam-casa.ma>
2024-11-27 15:42:00 +0100

    Refactor Navbar to use array of route objects

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,20 @@
 import { Link } from "react-router-dom";
 
-const Navbar = ()  => {
-  console.log('RENDER');
+const routes = [
+  { name: "Timer", path: "/timer" },
+  { name: "Counter", path: "/counter" },
+];
+
+const Navbar = () => {
   return (
     <>
-      <Link to={"/timer"}>A</Link>
-      <Link to={"/counter"}>B</Link>
+      {routes.map((route) => (
+        <Link key={route.path} to={route.path}>
+          {route.name}
+        </Link>
+      ))}
     </>
   );
-}
+};
 
-export default Navbar;  
+export default Navbar;
