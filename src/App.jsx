a401dcbeb6ef306fc56876d3b686f3a406c2cb8e
bal-wal <balhaoui.walid@ensam-casa.ma>
2024-11-28 20:09:36 +0100

    Implement Counter styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from "react";
 import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import "./output.css";
+import "./input.css"
 
 function App() {
   const route = useLocation().pathname;
   const colors = {
     "/": { main: "#B9D8C9", nav: "#0C1B33", icon: "#0C1B33"},
     "/counter": { main: "#000000", nav: "#ffffff", icon: "#000000" },
-    "/weather": { main: "#75b4e3", nav: "#8fe0ff", icon: "#75b4e3" },
+    "/weather": { main: "#3B7E88", nav: "#8fe0ff", icon: "#75b4e3" },
   };
   console.log(route);
   return (
