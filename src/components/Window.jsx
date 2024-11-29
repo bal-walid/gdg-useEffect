199d6f3bd79d1a0d4f954e227ed2e8d9c1428452
bal-wal <balhaoui.walid@ensam-casa.ma>
2024-11-29 13:00:01 +0100

    Uncommit Window solution ahead of workshop

--- src/components/Window.jsx
+++ src/components/Window.jsx
@@ -2,15 +2,6 @@ import { useEffect, useState } from "react";
 
 const Window = () => {
   const [size, setSize] = useState({width: window.innerWidth, height: window.innerHeight});
-  const resize = () => {
-    setSize({ width: window.innerWidth, height: window.innerHeight });
-  };
-  useEffect(() => {
-    window.addEventListener("resize", resize);
-    return () => {
-      window.removeEventListener('resize', resize);
-    }
-  }, [])
   return (
     <div className="window">
       <p>{size.width} x {size.height}</p>
